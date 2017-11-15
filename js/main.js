cb879a43b042498f95a1e6ea40d08f92e1d31ea1
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-11-15 20:02:44 +1100

    User can type text and click'send' button; the text appears after the first message

--- js/main.js
+++ js/main.js
@@ -7,4 +7,10 @@ $(document).ready(function() {
     $("h4#statement").delay(1000).slideDown(1000);
   });
 
+  $("button#send_chat").on("click", function(){
+    var enterChat = $("input#entered_chat").val();
+    // console.log(enterChat);
+    $('<h4>Nicholas: "' + enterChat + '"</h4>').appendTo("h4#user-statement");
+  });
+
 }); // END.ready function
