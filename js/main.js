a541b6c4b2e08a1ce6447e60da654a24e60152c7
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-11-15 19:55:40 +1100

    button 'chat now' - 1st message appears; added CSS for message to appear in the correct place

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-console.log("js file connected");
+// console.log("js file connected");
 
 
 $(document).ready(function() {
+
+  $("#firstBtn").on("click", function() {
+    $("h4#statement").delay(1000).slideDown(1000);
+  });
 
 }); // END.ready function
